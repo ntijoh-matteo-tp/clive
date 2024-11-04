7609f2b3e0298810efd9c632ce5308049810aace
MatteoTP06 <matteo.torquatoperillo@elev.ga.ntig.se>
2024-11-04 10:17:18 +0100

    04/11/2024

--- Gemfile
+++ Gemfile
@@ -6,3 +6,9 @@ source "https://rubygems.org"
 gem "minitest", "~> 5.25"
 
 gem "minitest-reporters", "~> 1.7"
+
+gem 'rubocop', require: false
+
+gem "rubycritic", require: false
+
+gem 'simplecov', require: false, group: :test
--- lib/request.rb
+++ lib/request.rb
@@ -1,13 +1,44 @@
-class Request
-  def initialize(requestString)
-    requestStringSplit = requestString.split(' ')
-    p requestStringSplit
-    @method, @resource, @version = requestStringSplit
-    p requestStringSplit.drop(3)
+# frozen_string_literal: true
 
-    pp "Method: " + @method, "Resource: " + @resource, "Version: " + @version
+# abc
+class Request
+  def initialize(requeststring)
+    @method, @resource, @version = requeststring.split(' ')
+    @headers = {}
+    request_string_split = requeststring.split("\n").drop(1)
+    request_string_split.map do |item| 
+      if item == ""
+        break
+      end
+
+      item = item.split(' ')
+      @headers[item[0].chomp(':')] = item[1]
+    end
+
+    @params = {}
+    def map_params(params)
+      params.map do |item|
+        item = item.split('=')
+        @params[item[0]] = item[1]
+      end
+    end
+
+    # Params in resource
+    if @resource.include?("?")
+      params = @resource.split('?').drop(1)
+      params = params[0].split('&')
+      map_params(params)
+    end
+
+    # Params at bottom
+    if request_string_split.include?("")
+      params = request_string_split[-1].split('&')
+      map_params(params)
+    end
+
+    puts "Method: #{@method}", "Resource: #{@resource}", "Version: #{@version}", "Headers: #{@headers}", "Params: #{@params}"
   end
 end
 
-requestString = File.read("../test/example_requests/get-examples.request.txt")
-Request.new(requestString)
+request_string = File.read('../test/example_requests/post-login.request.txt')
+Request.new(request_string)
